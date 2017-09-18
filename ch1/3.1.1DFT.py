dc18a2ea42f9a3a114f1e7179c8de3444a4c180f
vberthiaume <vberthiaume@gmail.com>
2017-09-18 10:50:26 -0400

    pretty printing

--- ch1/3.1.1DFT.py
+++ ch1/3.1.1DFT.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 
-def DFT(xn, N, m):
+def DFT(xn, N):
     #arrays = [xn*np.cos(2*np.pi*n*m/N) - 1j*xn*np.sin(2*np.pi*n*m/N) for n in np.arange(N)]
     #return np.sum(arrays, 1)
     Xm = []
-    for n in np.arange(N):
-        Xm.append(xn[n]*np.cos(2*np.pi*n*m/N) - 1j*xn[n]*np.sin(2*np.pi*n*m/N))
+    for m in np.arange(N):
+        xm = 0
+        for n in np.arange(N):
+            xm += xn[n]*np.cos(2*np.pi*n*m/N) - 1j*xn[n]*np.sin(2*np.pi*n*m/N)    
+        Xm.append(xm)
         
-    return np.sum(Xm, 0)
+    return Xm
 
 
 fo = 1000
@@ -21,5 +24,6 @@ xn = np.sin(2*np.pi*fo*n/fs) + .5*np.sin(2*np.pi*2*fo*n/fs + 3*np.pi/4)
 print "xn:"
 print xn
 
-print "DFT(xn, N, 1):"
-print DFT(xn, N, 1)
+Xn = DFT(xn, N)
+for xn in Xn:
+    print('{:.4f}'.format(xn))
