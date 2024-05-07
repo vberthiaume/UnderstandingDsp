c257468fb96c2cff9dfa64b5153415d00c1440ce
Vincent <vberthiaume@gmail.com>
2024-05-07 08:21:43 -0400

    faff

--- ch1/1.5.py
+++ ch1/1.5.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # make an array for n that goes from 0 to 31
-nMax = 32
-n = np.arange(nMax)
+numCycles = 2
+
+numPoints = 32
+n = np.arange(numPoints)
 # print (n)
 
 # the equation I'm trying to plot is this one 
@@ -29,27 +31,3 @@ plt.plot (n, x, 'o')
 plt.xlabel ('n')
 plt.ylabel ('x')
 plt.show()
-
-#1.6
-#x1 = np.cos(np.pi*n)   
-#x2 = np.cos((np.pi*n)/2)   
-#x3 = [np.cos(0) for x in n]
-
-#1.7
-# x1 = [0 if abs(np.sin(np.pi*x)) < 1e-5 else np.sin(np.pi*x) for x in n]
-# x2 = np.sin((np.pi*n)/2)   
-# x3 = [np.sin(0) for x in n]
-
-# # display the thing
-# fig, axarr = plt.subplots(3, sharex=False)
-
-# axarr[0].plot(n, x1)
-# axarr[0].set_title('x1')
-    
-# axarr[1].plot(n, x2)
-# axarr[1].set_title('x2')
-
-# axarr[2].plot(n, x3)
-# axarr[2].set_title('x3')
-
-# plt.show()
