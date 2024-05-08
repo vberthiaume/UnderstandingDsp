04b2f2aed06dc02615161c328a3d12bc9cc2fdec
Vincent <vberthiaume@gmail.com>
2024-05-08 08:12:19 -0400

    clenaup

--- ch1/1.5.py
+++ ch1/1.5.py
@@ -1,29 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# make an array for n
-numCycles = 3
-numPointsPerCycle = 32
+# make an array for n, which are the time points shown on the x axis
+numCycles = 3                               #how many repetitions of the signal below
+numPointsPerCycle = 32                      #how many sample points in one cycle of the signal
 numPoints = numCycles * numPointsPerCycle
-n = np.arange(numPoints)
+n = np.arange (numPoints)
 # print (n)
 
-# the equation I'm trying to plot is this one 
-# x(n) = sin (2 * pi * f * n * ts)
-
-# and ts is the sampling period in seconds, that is how much time between samples,
-# which is the inverse of the sampling frequency fs: fs = 1/ts
-# here we have 32 samples per cycle, which means
-# hertz are cycles per seconds
-
-f = 2   # f is the frequency of the sine wave
-ts = 1 / numPointsPerCycle  # time period between samples
+f = 2                       # f is the frequency of my signal
+ts = 1 / numPointsPerCycle  # time period in seconds between samples
 
 x = np.sin (2*np.pi * f * n * ts)
 
-# doc for plot: https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
-# 'bo' here is b for blue and o for circle. See the notes section
-plt.plot (n, x, 'o')
+# doc for plot, esp the string arg: https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
+plt.plot (n, x, '.')
 plt.xlabel ('n')
 plt.ylabel ('x')
 plt.show()
