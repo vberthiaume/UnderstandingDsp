3094577ccc96919db1eba9bf63e0d00c029ff437
Vincent <vberthiaume@gmail.com>
2024-06-08 09:01:56 -0400

    cleanup

--- ch1/problems/1.19.py
+++ ch1/problems/1.19.py
@@ -3,15 +3,12 @@ from scipy.ndimage import shift
 
 a, b, delay = 3, 5, 1
 
-x1 = np.arange (10)
-x1 *= a
-x1delay = shift (x1, delay, cval=0)
-x1delay *= b
+x1 = np.arange (10) * a
+x1delay = shift (x1, delay, cval=0) * b
 y1 = x1 + x1delay
 
 x2 = np.arange (10)
-x2delay = shift (x2, delay, cval=0)
-x2delay *= b
+x2delay = shift (x2, delay, cval=0) * b
 y2 = a * (x2 + x2delay)
 
 print (y1)
