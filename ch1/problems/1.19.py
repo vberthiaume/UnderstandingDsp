245225dcd69f6d4f04f0696210916e128398663d
Vincent <vberthiaume@gmail.com>
2024-06-08 08:45:24 -0400

    1.19

---
+++ ch1/problems/1.19.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy.ndimage import shift
+
+a, b, delay = 3, 5, 1
+
+x1 = np.arange (10)
+x1 *= a
+x1delay = shift (x1, delay, cval=0)
+x1delay *= b
+y1 = x1 + x1delay
+
+x2 = np.arange (10)
+x2delay = shift (x2, delay, cval=0)
+x2delay *= b
+y2 = a * (x2 + x2delay)
+
+print (y1)
+print (y2)
