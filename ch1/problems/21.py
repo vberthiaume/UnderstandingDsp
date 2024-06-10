92b5878026811c87862551488982fb3c59f027ae
Vincent <vberthiaume@gmail.com>
2024-06-10 08:26:56 -0400

    still trying to figure out problem 1.21

--- ch1/problems/21.py
+++ ch1/problems/21.py
@@ -11,22 +11,26 @@ from scipy.ndimage import shift
 num = 10
 n = np.arange (num)
 x = np.zeros (num)
-x[2] = 1
+x[0] = 1
 y = np.zeros (num)
 
 # (a) a 4th-order comb filter: y(n) = x(n) – x(n–4),
 ya = x - shift (x, 4)
 
 # (b) an integrator: y(n) = x(n) + y(n–1),
-yb = x;
+yb = x
 yb += shift (yb, 1)
 
 # (c) a leaky integrator: y(n) = Ax(n) + (1–A)y(n–1) [the scalar value A is a real-valued constant in the range 0 <A<1],
-# (d) a differentiator: y(n) = 0.5x(n) – 0.5x(n-2)
+A = .5
+yc = A * x
+yc += (1-A) * shift (yc, 1)
 
+# (d) a differentiator: y(n) = 0.5x(n) – 0.5x(n-2)
+yd = (.5 * x) - (.5 * shift (x, 2))
 
 # display the thing
-fig, axarr = plt.subplots(2, sharex=False)
+fig, axarr = plt.subplots (4, sharex=False)
 
 axarr[0].plot(n, ya, '.')
 axarr[0].set_title('ya')
@@ -34,10 +38,10 @@ axarr[0].set_title('ya')
 axarr[1].plot(n, yb, '.')
 axarr[1].set_title('yb')
 
-# axarr[2].plot(n, y)
-# axarr[2].set_title('y')
+axarr[2].plot(n, yc, '.')
+axarr[2].set_title('yc')
 
-# axarr[3].plot(xf, yf, '.')
-# axarr[3].set_title('y fft')
+axarr[3].plot(n, yd, '.')
+axarr[3].set_title('yd')
 
 plt.show()
